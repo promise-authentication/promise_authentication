5e3a2c9f7517f0ba057cffd61550e2d55d578858
anderslemke <mail@anderslemke.dk>
2020-04-08 10:04:29 +0200

    Get that encrypted email right

--- app/models/authentication/encrypted_email.rb
+++ app/models/authentication/encrypted_email.rb
@@ -4,17 +4,21 @@ class Authentication::EncryptedEmail < ApplicationRecord
   validates :user_id, presence: true
 
   def self.from_cleartext(email)
+    return nil if email.nil?
+
     # Double hashing:
     # To avoid collisions, the assumption is, that if we
     # are unlucky enough to get a collision in one algorithm,
     # we should not be so unlucky that it also collides in the other...
     # I'm sure, though, that I will be proven wrong at some point.
-    (RbNaCl::Hash.sha256(email) + RbNaCl::Hash.blake2b(email)).force_encoding('UTF-8')
+    (RbNaCl::Hash.sha256(email) + RbNaCl::Hash.blake2b(email)).force_encoding(Encoding::UTF_8)
   end
 
   def self.user_id_for_cleartext(email)
     id = from_cleartext(email)
 
-    find(id)&.user_id
+    find(id).user_id
+  rescue ActiveRecord::RecordNotFound
+    return nil
   end
 end
--- test/models/authentication/encrypted_email_test.rb
+++ test/models/authentication/encrypted_email_test.rb
@@ -29,6 +29,16 @@ class Authentication::EncryptedEmailTest < ActiveSupport::TestCase
     end
   end
 
+  test 'from_cleartext handles nil' do
+    assert_nil @described_class.from_cleartext(nil)
+  end
+
+  test 'returns UTF-8 encoded string' do
+    string = @described_class.from_cleartext('hello')
+    assert_equal Encoding::UTF_8, string.encoding
+    assert_equal ",\xF2M\xBA_\xB0\xA3\u000E&\xE8;*Ź\xE2\x9E\e\u0016\u001E\\\u001F\xA7B^s\u00043b\x93\x8B\x98$\xE4ϣ\x9A=7\xBE1Ŗ\t\xE8\a\x97\a\x99ʦ\x8A\u0019\xBF\xAA\u0015\u0013_\u0016P\x85\xE0\u001DA\xA6[\xA1\xE1\xB1F\xAE\xB6\xBD\u0000\x92\xB4\x9E\xAC!L\u0010<ϣ\xA3e\x95K\xBB\xE5/t\xA2\xB3b\f\x94", string
+  end
+
   test 'from_cleartext' do
     email1 = 'hello@example.com'
     email2 = 'hello@world.com'
@@ -42,6 +52,7 @@ class Authentication::EncryptedEmailTest < ActiveSupport::TestCase
   end
 
   test 'user_id_for_cleartext when not present' do
+    assert_nil @described_class.user_id_for_cleartext('someone@notthere.com')
   end
 
   test 'user_id_for_cleartext when present' do
