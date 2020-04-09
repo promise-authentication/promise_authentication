a848392b4b9518e8adae0d89c6946f713dcde96d
anderslemke <mail@anderslemke.dk>
2020-04-09 13:57:51 +0200

    Add controller test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,15 @@ class ApplicationController < ActionController::Base
     return nil if session[:user_id].blank?
 
     @personal_data ||= -> do
-      encrypted = Authentication::VaultContent.find(session[:user_id]).encrypted_personal_data
-      decrypted = Authentication::Vault.new(key: session[:vault_key]).decrypt encrypted
-      Authentication::PersonalData.new(
-        decrypted
-      )
+      encrypted = Authentication::VaultContent.
+        find(session[:user_id]).
+        encrypted_personal_data
+
+      decrypted = Authentication::Vault.
+        new(key: session[:vault_key]).
+        decrypt encrypted
+
+      Authentication::PersonalData.new(decrypted)
     end.call
   end
   helper_method :personal_data
--- test/controllers/humans_controller_test.rb
+++ test/controllers/humans_controller_test.rb
@@ -13,4 +13,20 @@ class HumansControllerTest < ActionDispatch::IntegrationTest
     post authenticate_url
     assert_response :redirect
   end
+
+  test 'authentication with something' do
+    service = Minitest::Mock.new
+    service.expect :valid?, true
+    service.expect :user_id_and_salt, ['uid', 'salt']
+    service.expect :password, 'secret'
+    Authentication::Vault.stub :key_from, 'key' do
+      Authentication::Services::Authenticate.stub :new, service do
+        post authenticate_url, params: { email: 'hello@world.com', password: 'secret' }
+
+        assert_mock service
+        assert_equal session[:user_id], 'uid'
+        assert_equal session[:vault_key], 'key'
+      end
+    end
+  end
 end
