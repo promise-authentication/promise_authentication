0a2bc38c7c0ae8baf740bac3860ee3ef85c6125f
anderslemke <mail@anderslemke.dk>
2020-04-10 15:27:00 +0200

    Add autoprefixer

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: [
     require('tailwindcss')('./app/javascript/css/tailwind.js'),
+    require('autoprefixer'),
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
