a6aeea07b2a85a000b1808ada7712d9cc3066edf
anderslemke <mail@anderslemke.dk>
2020-04-24 21:25:06 +0200

    Include new decrypt info

--- app/domain/authentication/events/password_set.rb
+++ app/domain/authentication/events/password_set.rb
@@ -3,7 +3,7 @@ class Authentication::Events::PasswordSet < DomainEvent
     uniq_key_pair = Authentication::KeyPair.find(data[:encrypted_vault_key][:key_pair_id])
 
     return {
-      private_key_cipher: uniq_key_pair.private_key_cipher_base64,
+      public_key_base64: uniq_key_pair.public_key_base64,
       vault_key_cipher: data[:encrypted_vault_key][:cipher_base64]
     }
   end
