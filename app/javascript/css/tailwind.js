3ef1ee9b0e5feeed783b85f78411494f66c4d1eb
anderslemke <mail@anderslemke.dk>
2020-05-04 15:42:14 +0200

    Update Tailwind

--- app/javascript/css/tailwind.js
+++ app/javascript/css/tailwind.js
@@ -1,4 +1,7 @@
 module.exports = {
+  purge: [
+    "../../views/**/*.html.erb"
+  ],
   theme: {
     extend: {},
   },
--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   plugins: [
-    require('tailwindcss')('./app/javascript/css/tailwind.js'),
-    require('autoprefixer'),
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
+    require('tailwindcss')('./app/javascript/css/tailwind.js'),
+    require('autoprefixer'),
     require('postcss-preset-env')({
       autoprefixer: {
         flexbox: 'no-2009'
