a9d3f94a9ee7c5d8d970df1b7310e01c9ced7a0d
anderslemke <mail@anderslemke.dk>
2020-05-05 21:10:50 +0200

    Only one worker

--- config/initializers/sneakers.rb
+++ config/initializers/sneakers.rb
@@ -1,10 +1,12 @@
 require 'sneakers/handlers/maxretry'
 require 'sneakers'
-require 'airbrake/sneakers'
 
 Sneakers.configure(
   handler: Sneakers::Handlers::Maxretry,
   amqp: ENV['CLOUDAMQP_URL'] || 'amqp://guest:guest@localhost:5672',
+  prefetch: 1,
+  threads: 1,
+  workers: 1,
   exchange_options: { durable: true },
 )
 
