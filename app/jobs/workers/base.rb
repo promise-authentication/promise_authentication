9e138c52efec79c580013f3742d01fbda120225c
anderslemke <mail@anderslemke.dk>
2020-05-06 21:43:33 +0200

    Simplify that worker

--- app/jobs/workers/base.rb
+++ app/jobs/workers/base.rb
@@ -1,19 +1,2 @@
-class Workers::Base
-  include Sneakers::Worker
-
-  def self.queue_opts
-    {
-      durable: true,
-      ack: true,
-      arguments: {
-        :'x-dead-letter-exchange' => "#{queue_name}-retry"
-      }
-    }
-  end
-
-  def work(msg)
-    job_data = ActiveSupport::JSON.decode(msg)
-    ActiveJob::Base.execute job_data
-    ack!
-  end
+class Workers::Base < ActiveJob::QueueAdapters::SneakersAdapter::JobWrapper
 end
