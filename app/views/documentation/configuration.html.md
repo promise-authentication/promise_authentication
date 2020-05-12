44898967aaa8fbfda70b8e7a080bf7050639ce5a
anderslemke <mail@anderslemke.dk>
2020-05-12 20:25:41 +0200

    Remove those unused files

--- app/views/documentation/configuration.html.md
+++
@@ -1,6 +0,0 @@
-# Configuration
-
-<a name="redirect_to">`redirect_to`</a><a href="#redirect_to"><%= internal_icon %></a>
-
-You can have <%= name %> redirect to any URL. To do this, you also has to provide the URL in your whitelisted redirect URLs in your [well-knowns](<%= documentation_well_known_path %>).
-
--- config/routes.rb
+++ config/routes.rb
@@ -9,12 +9,7 @@ Rails.application.routes.draw do
     get 'pricing', as: 'pricing'
   end
 
-  namespace 'documentation' do
-    get 'get_started'
-    get 'well_known'
-    get 'configuration'
-  end
-
+  # This is also the getting started part
   namespace 'admin' do
     resources :relying_parties, constraints: { id: /[^\/]+/ }, only: [:show, :index]
   end
@@ -24,16 +19,16 @@ Rails.application.routes.draw do
     resources :relying_parties, constraints: { id: /[^\/]+/ }, only: [:index]
   end
 
-  get 'a/:client_id', to: 'authentication#login', as: 'login_with_relying_party', constraints: { client_id: /[^\/]+/ }
-  get 'a', to: 'authentication#login', as: 'login_short'
-
-  get 'login', to: 'authentication#login', as: 'login'
+  # Paths for basic login functionality
+  get    'a/:client_id', to: 'authentication#login', as: 'login_with_relying_party', constraints: { client_id: /[^\/]+/ }
+  get    'a', to: 'authentication#login', as: 'login_short'
+  get    'login', to: 'authentication#login', as: 'login'
   delete 'logout', to: 'authentication#logout', as: 'logout'
   delete 'relogin', to: 'authentication#relogin', as: 'relogin'
-  get 'confirm', to: 'authentication#confirm', as: 'confirm'
-  post 'authenticate', to: 'authentication#authenticate', as: 'authenticate'
-  get 'authenticate', to: 'authentication#login'
-  post 'go_to', to: 'authentication#go_to', as: 'go_to'
+  get    'confirm', to: 'authentication#confirm', as: 'confirm'
+  post   'authenticate', to: 'authentication#authenticate', as: 'authenticate'
+  get    'authenticate', to: 'authentication#login'
+  post   'go_to', to: 'authentication#go_to', as: 'go_to'
 
   resource :password, only: [:new, :show, :create] do
     member do
@@ -48,7 +43,6 @@ Rails.application.routes.draw do
 
   get 'me', to: 'humans#show', as: 'dashboard'
   get 'uniq', to: 'humans#uniq', as: 'uniq'
-
 
   scope path: '.well-known' do
     get 'jwks', to: 'well_knowns#jwks'
