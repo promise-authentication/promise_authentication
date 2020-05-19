d1d2617497543c34604ae5ac2694ea8c0ec5be21
anderslemke <mail@anderslemke.dk>
2020-05-19 20:08:45 +0200

    This is forbidden. I'm sorry!

--- db/migrate/20200408144448_create_authentication_passwords.rb
+++ db/migrate/20200408144448_create_authentication_passwords.rb
@@ -2,7 +2,7 @@ class CreateAuthenticationPasswords < ActiveRecord::Migration[6.0]
   def change
     create_table :authentication_passwords, id: :string do |t|
       t.binary :digest
-      t.string :vault_key_salt
+      t.binary :vault_key_salt
       t.timestamps
     end
   end
