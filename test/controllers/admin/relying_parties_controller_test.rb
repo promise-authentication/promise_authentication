d41ba4ef45a0c02522cdbd5a076dc44111cccc3d
anderslemke <mail@anderslemke.dk>
2020-05-30 15:25:26 +0200

    Add test for configuration

--- test/controllers/admin/relying_parties_controller_test.rb
+++ test/controllers/admin/relying_parties_controller_test.rb
@@ -2,4 +2,9 @@ require 'test_helper'
 
 class Admin::RelyingPartiesControllerTest < ActionDispatch::IntegrationTest
 
+  test 'show' do
+    get '/admin/relying_parties/example.com'
+    assert_response :success
+  end
+
 end
