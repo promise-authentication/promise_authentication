8fd7a81ced35ce90c4273d5fa2651c081592e0fd
Anders Lemke-Holstein <anders@lemke.dk>
2023-03-24 13:29:32 +0100

    🚨 Update faraday and use prose

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ require("turbolinks").start()
 // require("@rails/activestorage").start()
 // require("channels")
 
-// import '../css/markdown.scss'
 // import '../css/tailwind.css'
 // import '../css/input.scss'
 // import '../css/buttons.scss'
