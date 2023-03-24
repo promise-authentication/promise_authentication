6587994614c8d21395196fe3b5d10a7b48e25af8
Anders Lemke-Holstein <anders@lemke.dk>
2023-03-24 12:00:46 +0100

    ♻️ 🤷

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,12 +8,12 @@ require("turbolinks").start()
 // require("@rails/activestorage").start()
 // require("channels")
 
-import '../css/markdown.scss'
-import '../css/tailwind.css'
-import '../css/input.scss'
-import '../css/buttons.scss'
-import '../css/globes.scss'
-import '../css/animations.scss'
+// import '../css/markdown.scss'
+// import '../css/tailwind.css'
+// import '../css/input.scss'
+// import '../css/buttons.scss'
+// import '../css/globes.scss'
+// import '../css/animations.scss'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
