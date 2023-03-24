42882557d8051b79f803ceddf61fdaa0107e6eca
Anders Lemke-Holstein <anders@lemke.dk>
2023-03-24 10:17:12 +0100

    WIP

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '2.6.5'
+ruby '3.0.2'
 
 # Event store
 gem "rails_event_store"
@@ -31,7 +31,7 @@ gem 'chartkick'
 gem 'groupdate'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 6.1.3'
+gem 'rails', '~> 6.1.7.3'
 # Use Puma as the app server
 gem 'puma', '~> 5.2'
 # Use SCSS for stylesheets
@@ -90,3 +90,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem "activeresource", "~> 5.1"
 
 gem "webmock", "~> 3.12"
+
+# gem "matrix", "~> 0.4.2"
--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_19_142555) do
+ActiveRecord::Schema.define(version: 2020_05_18_145122) do
 
   create_table "ahoy_events", force: :cascade do |t|
     t.integer "visit_id"
@@ -109,7 +109,7 @@ ActiveRecord::Schema.define(version: 2020_05_19_142555) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "event_store_events", id: :string, limit: 36, force: :cascade do |t|
+  create_table "event_store_events", id: { type: :string, limit: 36 }, force: :cascade do |t|
     t.string "event_type", null: false
     t.binary "metadata"
     t.binary "data", null: false
