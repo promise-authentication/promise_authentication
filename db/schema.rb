75fc59ff303553fb2240e315c591efbf29d3f65f
Anders Lemke-Holstein <anders@lemke.dk>
2025-04-11 17:11:50 +0200

    Docker and small ui update

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2020_05_18_145122) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "ahoy_events", force: :cascade do |t|
     t.integer "visit_id"
     t.integer "user_id"
