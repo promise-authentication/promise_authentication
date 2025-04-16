da96b7fc62a7493c39de3fdd7530c19d34d77440
Anders Lemke-Holstein <anders@lemke.dk>
2025-04-16 14:03:26 +0200

    ✅ More tests

--- test/domain/authentication/services/prepare_email_for_validation_test.rb
+++ test/domain/authentication/services/prepare_email_for_validation_test.rb
@@ -18,5 +18,25 @@ class Authentication::Services::PrepareEmailForValidationTest < ActiveSupport::T
 
     assert_equal inst.verify!(code.code), true
     assert_equal inst.verify!('dakj'), false
+
+    # Test that the email was sent:
+    email = ActionMailer::Base.deliveries.last
+    assert_not_nil email
+    assert_includes email.body.encoded, code.code
+
+    # When I call it again, it should reset and send new mail
+    inst.generate_and_send_verification_code!
+    new_code = EmailVerificationCode.last
+    assert_equal EmailVerificationCode.count, 1
+    assert_equal EmailVerificationCode.find_by_cleartext('hello@world.com').code, new_code.code
+
+    assert_not_equal code.code, new_code.code
+    assert_not_equal code.created_at, new_code.created_at
+
+    new_email = ActionMailer::Base.deliveries.last
+    assert_not_nil new_email
+    assert_includes new_email.body.encoded, new_code.code
+
+    assert_equal ActionMailer::Base.deliveries.size, 2
   end
 end
