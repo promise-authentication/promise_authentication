d4672b8207bce44799595de29b442db860f8dfab
Anders Lemke-Holstein <anders@lemke.dk>
2025-04-16 13:34:10 +0200

    🔒️Better random

--- app/domain/authentication/email.rb
+++ app/domain/authentication/email.rb
@@ -6,7 +6,6 @@ class Authentication::Email
   def initialize(hashed_email)
     @hashed_email = hashed_email
     @claimed = false
-    @validated = false
   end
 
   def claim(user_id:, email_verified_at: nil)
--- app/models/email_verification_code.rb
+++ app/models/email_verification_code.rb
@@ -3,10 +3,10 @@ class EmailVerificationCode < ApplicationRecord
     ALPHABET = 'ABCDEFGHKMNPQRSTUVWXYZ23456789'.freeze
     def self.generate(range)
       # The length should be a random number in the range
-      length = range.to_a.sample
+      length = range.to_a.sample(1, random: SecureRandom).first
       # The code should be a random string of the given length
       # sampled from the alphabet
-      Array.new(length) { ALPHABET.chars.sample }.join
+      Array.new(length) { ALPHABET.chars.sample(1, random: SecureRandom).first }.join
     end
   end
 
