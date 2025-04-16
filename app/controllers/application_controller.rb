52c012b9c951983b01ad16a7636b7b5190571a96
Anders Lemke-Holstein <anders@lemke.dk>
2025-04-16 21:44:03 +0200

    ♻️ Move it into a concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,23 +3,7 @@ class ApplicationController < ActionController::Base
   include AuthenticatedConcern
   include RelyingPartyConcern
   include TurnstileConcern
+  include BackToConcern
 
   protect_from_forgery with: :reset_session
-
-  before_action :move_back
-
-  helper_method :back_to
-
-  def back_to(params)
-    with_back = params.merge(back: true)
-    url = yield with_back if block_given?
-    helpers.link_to '&larr; '.html_safe + t('.cancel'), url, class: 'text-secondary pr-3'
-  end
-
-  def move_back
-    return unless params[:back]
-
-    flash[:slide_class] = 'a-slide-in-from-left'
-    redirect_to url_for(params.clone.permit!.except(:back).merge(only_path: true))
-  end
 end
